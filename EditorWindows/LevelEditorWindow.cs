319b152f21adc914274e36053462b6ced974863b
Linkz <39253822+Linkz64@users.noreply.github.com>
2025-09-19 06:59:46 +0100

    Custom viewport camera

--- EditorWindows/LevelEditorWindow.cs
+++ EditorWindows/LevelEditorWindow.cs
@@ -1,11 +1,13 @@
 ﻿using IceSaw2.LevelObject;
 using IceSaw2.LevelObject.TrickyObjects;
 using IceSaw2.Manager;
+using IceSaw2.Settings;
 using ImGuiNET;
 using Raylib_cs;
 using rlImGui_cs;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Numerics;
 using System.Text;
@@ -16,33 +18,35 @@ namespace IceSaw2.EditorWindows
 {
     public class LevelEditorWindow : BaseEditorWindow
     {
-        Camera3D worldCamera3D = new Camera3D();
+        private float yaw = 0.0f;
+        private float pitch = 0.0f;
+        private float mouseSensitivity = 0.003f;
+        private float moveSpeed = 0.1f;
+        private const float moveSpeedStep = 0.008f;
+        private int screenWidth;
+        private int screenHeight;
+
+        Camera3D camera3D = new Camera3D();
         public bool Open = true;
+
+
         public void Initilize()
         {
-            worldCamera3D.Position = new System.Numerics.Vector3(0, 100, 100);
-            worldCamera3D.Target = Vector3.Zero;
-            worldCamera3D.Up = new Vector3(0, 0, 1);
-            worldCamera3D.FovY = 65f;
-            worldCamera3D.Projection = CameraProjection.Perspective;
+            camera3D.Position = new System.Numerics.Vector3(0, 100, 100);
+            camera3D.Target = Vector3.Zero;
+            camera3D.Up = new Vector3(0, 0, 1);
+            camera3D.FovY = 65f;
+            camera3D.Projection = CameraProjection.Perspective;
 
-            
+            screenWidth = GeneralSettings.ScreenWidth;
+            screenHeight = GeneralSettings.ScreenHeight;
         }
 
         public override void RenderUpdate()
         {
             //Render 3D
-            Raylib.BeginMode3D(worldCamera3D);
+            Raylib.BeginMode3D(camera3D);
             Rlgl.DisableBackfaceCulling();
-
-            if (Raylib.IsMouseButtonDown(MouseButton.Right))
-            {
-                Raylib.DisableCursor();
-            }
-            if (Raylib.IsMouseButtonReleased(MouseButton.Right))
-            {
-                Raylib.EnableCursor();
-            }
 
 
             Rlgl.DisableDepthMask();
@@ -51,11 +55,57 @@ namespace IceSaw2.EditorWindows
             {
                 var TempLocation = DataManager.trickySkyboxPrefabObjects[i].Position;
 
-                DataManager.trickySkyboxPrefabObjects[i].Position = worldCamera3D.Position / BaseObject.WorldScale;
+                DataManager.trickySkyboxPrefabObjects[i].Position = camera3D.Position / BaseObject.WorldScale;
                 DataManager.trickySkyboxPrefabObjects[i].Render();
                 DataManager.trickySkyboxPrefabObjects[i].Position = TempLocation;
             }
             Rlgl.EnableDepthMask();
+
+
+            // Viewport Camera
+            if (Raylib.IsMouseButtonDown(MouseButton.Right))
+            {
+                Raylib.HideCursor();
+
+                // Look
+                Vector2 mouseDelta = Raylib.GetMouseDelta();
+                yaw += mouseDelta.X * mouseSensitivity;
+                pitch -= mouseDelta.Y * mouseSensitivity;
+                pitch = Math.Clamp(pitch, -1.5f, 1.5f);
+
+                Raylib.SetMousePosition(screenWidth / 2, screenHeight / 2);
+
+                Vector3 forward = new Vector3(MathF.Cos(pitch) * MathF.Sin(yaw), MathF.Cos(pitch) * MathF.Cos(yaw), MathF.Sin(pitch));
+                Vector3 right = new Vector3(MathF.Sin(yaw - MathF.PI / 2f), MathF.Cos(yaw - MathF.PI / 2f), 0f);
+
+                // Movement
+                if (Raylib.IsKeyDown(KeyboardKey.W)) camera3D.Position += forward * moveSpeed;
+                if (Raylib.IsKeyDown(KeyboardKey.S)) camera3D.Position -= forward * moveSpeed;
+                if (Raylib.IsKeyDown(KeyboardKey.D)) camera3D.Position -= right * moveSpeed;
+                if (Raylib.IsKeyDown(KeyboardKey.A)) camera3D.Position += right * moveSpeed;
+                if (Raylib.IsKeyDown(KeyboardKey.E)) camera3D.Position.Z += moveSpeed;
+                if (Raylib.IsKeyDown(KeyboardKey.Q)) camera3D.Position.Z -= moveSpeed;
+
+                float wheel = Raylib.GetMouseWheelMove();
+                if (wheel != 0)
+                {
+                    moveSpeed += wheel * moveSpeedStep;
+                    moveSpeed = Math.Clamp(moveSpeed, 0.008f, 200f);
+                    Debug.WriteLine(moveSpeed);
+                }
+                
+
+                camera3D.Target = camera3D.Position + forward;
+            }
+            if (Raylib.IsMouseButtonReleased(MouseButton.Right))
+            {
+                Raylib.ShowCursor();
+            }
+            
+
+
+
+            
 
             //Render Default
             Raylib.DrawLine3D(new Vector3(-1000, 0, 0), new Vector3(1000, 0, 0), new Color(212, 28, 4));
@@ -109,16 +159,11 @@ namespace IceSaw2.EditorWindows
             //Raylib.DrawTexture(skyboxTexture2Ds[0], 100, 100, Color.White);
         }
 
-        float cameraSpeed = 0.1f;
-        float mouseSensitivity = 0.003f;
-        float yaw = 0.0f;
-        float pitch = 0.0f;
-
-        public override void LogicUpdate()
-        {
-            //Update Camera
-            Raylib.UpdateCamera(ref worldCamera3D, CameraMode.Free);
-        }
+        //public override void LogicUpdate()
+        //{
+        //    //Update Camera
+        //    Raylib.UpdateCamera(ref camera3D, CameraMode.Free);
+        //}
 
 
     }
