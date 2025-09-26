d08e0d0e0f7f1db9b4b8ca6fbca375bfd1b1d731
GlitcherOG <54088472+GlitcherOG@users.noreply.github.com>
2025-09-27 09:13:25 +1000

    Create EditorJsonHandler.cs
    Dummy file not used atm

---
+++ JsonFiles/EditorJsonHandler.cs
@@ -0,0 +1,41 @@
+﻿using Raylib_cs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static IceSaw2.Manager.WorldManager;
+
+namespace IceSaw2.JsonFiles
+{
+    [System.Serializable]
+    public class EditorJsonHandler
+    {
+        //Archys Dummy file to figure out what would need to be saved and how to save it
+        //TBD if used
+
+        public int MajorVersion;
+        public int MinorVersion;
+
+        public Viewport LevelViewport;
+        public TreeNode TreeNodes;
+
+        public WindowMode ActiveWindow;
+        public int SelectionNodeID; //Swapped out for list maybe?
+
+        public struct Viewport
+        {
+
+        }
+        
+
+        public struct TreeNode
+        {
+            public string Name;
+            public int NodeID;
+            public int ObjectID; //-1 is no tricky object
+
+            public List<TreeNode> treeNodes;
+        }
+    }
+}
