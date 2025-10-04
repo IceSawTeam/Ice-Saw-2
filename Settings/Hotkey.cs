cd55ca80d46ea103dd835b046437512a771cd645
Erickson <claudespeed400@gmail.com>
2025-10-04 17:38:10 -0400

    Settings overhaul

---
+++ Settings/Hotkey.cs
@@ -0,0 +1,64 @@
+﻿using Newtonsoft.Json;
+using Raylib_cs;
+using System.Diagnostics;
+
+
+namespace IceSaw2.Settings
+{
+    public class Hotkey
+    {
+        private Hotkey() { }
+        private static readonly Hotkey _instance = new();
+        public static Hotkey Instance { get { return _instance; } }
+
+        public DataClass data = new();
+        public class DataClass
+        {
+            //TODO: Convert to use a list so we can have multi hotkeys
+            //General
+            public KeyboardKey LevelWindow = KeyboardKey.L;
+            public KeyboardKey MaterialWindow = KeyboardKey.M;
+            public KeyboardKey PrefabWindow = KeyboardKey.P;
+            public KeyboardKey OpenProject = KeyboardKey.F;
+
+            // Camera movement
+            public MouseButton ActivateCamera = MouseButton.Right;
+            public KeyboardKey Forward = KeyboardKey.W;
+            public KeyboardKey Back = KeyboardKey.S;
+            public KeyboardKey Left = KeyboardKey.A;
+            public KeyboardKey Right = KeyboardKey.D;
+            public KeyboardKey Up = KeyboardKey.E;
+            public KeyboardKey Down = KeyboardKey.Q;
+            public KeyboardKey Boost = KeyboardKey.LeftShift;
+        }
+
+
+        public void Load()
+        {
+            string loadFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "IceSaw2");
+            string loadPath = Path.Combine(loadFolder, "Hotkeys.json");
+            if (File.Exists(loadPath))
+            {
+                string stream = File.ReadAllText(loadPath);
+                DataClass? loadedData = JsonConvert.DeserializeObject<DataClass>(stream);
+                Debug.Assert(loadedData != null, "Container is null");
+                data = loadedData;
+                return;
+            }
+            Instance.Save();
+            return;
+        }
+        
+        public void Save()
+        {
+            const bool PRETTY = true;
+            string saveFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "IceSaw2");
+            string savePath = Path.Combine(saveFolder, "Hotkeys.json");
+            var tempFormating = PRETTY ? Formatting.Indented : Formatting.None;
+            var serializer = JsonConvert.SerializeObject(data, tempFormating);
+            File.WriteAllText(savePath, serializer);
+        }
+
+
+    }
+}
