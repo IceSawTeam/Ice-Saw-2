738d412695fe5d009b62634975e40ec151faa5fe
GlitcherOG <54088472+GlitcherOG@users.noreply.github.com>
2025-10-06 09:31:13 +1100

    Instance Saving

--- LevelObject/TrickyObjects/TrickyInstanceObject.cs
+++ LevelObject/TrickyObjects/TrickyInstanceObject.cs
@@ -1,6 +1,7 @@
 using IceSaw2.LevelObject;
 using IceSaw2.LevelObject.TrickyObjects;
 using IceSaw2.Manager.Tricky;
+using Raylib_cs;
 using SSXMultiTool.JsonFiles.Tricky;
 using SSXMultiTool.Utilities;
 using System.Collections.Generic;
@@ -161,6 +162,135 @@ namespace IceSaw2.LevelObject
                 TrickyPrefab = TrickyDataManager.trickyPrefabObjects[ModelID];
                 GenerateRenderCache();
             }
+        }
+
+        public InstanceJsonHandler.InstanceJson GenerateInstance()
+        {
+            InstanceJsonHandler.InstanceJson TempInstance = new InstanceJsonHandler.InstanceJson();
+            TempInstance.InstanceName = Name;
+
+            TempInstance.Location = JsonUtil.Vector3ToArray(Position);
+            TempInstance.Scale = JsonUtil.Vector3ToArray(Scale);
+            TempInstance.Rotation = JsonUtil.QuaternionToArray(Rotation);
+
+            TempInstance.LightVector1 = JsonUtil.Vector4ToArray(JsonUtil.Vector3ToVector4(LightVector1, 0));
+            TempInstance.LightVector2 = JsonUtil.Vector4ToArray(JsonUtil.Vector3ToVector4(LightVector2, 0));
+            TempInstance.LightVector3 = JsonUtil.Vector4ToArray(JsonUtil.Vector3ToVector4(LightVector3, 0));
+            TempInstance.AmbentLightVector = JsonUtil.Vector4ToArray(JsonUtil.Vector3ToVector4(AmbentLightVector, 0));
+
+            TempInstance.LightColour1 = JsonUtil.Vector4ToArray(JsonUtil.Vector3ToVector4(LightColour1, 0));
+            TempInstance.LightColour2 = JsonUtil.Vector4ToArray(JsonUtil.Vector3ToVector4(LightColour2, 0));
+            TempInstance.LightColour3 = JsonUtil.Vector4ToArray(JsonUtil.Vector3ToVector4(LightColour3, 0));
+            TempInstance.AmbentLightColour = JsonUtil.Vector4ToArray(AmbentLightColour);
+
+            TempInstance.ModelID = ModelID;
+
+            //if (PrefabObject != null)
+            //{
+            //    TempInstance.ModelID = TrickyLevelManager.Instance.dataManager.GetPrefabID(PrefabObject);
+            //    //TempInstance.ModelID2 = TempInstance.ModelID;
+            //}
+            //else
+            //{
+            //    TempInstance.ModelID = -1;
+            //    //TempInstance.ModelID2 = -1;
+            //}
+
+            TempInstance.PrevInstance = PrevInstance;
+
+            //if (PrevInstanceObject != null)
+            //{
+            //    TempInstance.PrevInstance = TrickyLevelManager.Instance.dataManager.GetInstanceID(PrevInstanceObject);
+            //}
+            //else
+            //{
+            //    TempInstance.PrevInstance = -1;
+            //}
+
+            TempInstance.NextInstance = NextInstance;
+
+            //if (NextInstanceObject != null)
+            //{
+            //    TempInstance.NextInstance = TrickyLevelManager.Instance.dataManager.GetInstanceID(NextInstanceObject);
+            //}
+            //else
+            //{
+            //    TempInstance.NextInstance = -1;
+            //}
+
+            TempInstance.UnknownInt26 = UnknownInt26;
+            TempInstance.UnknownInt27 = UnknownInt27;
+            TempInstance.UnknownInt28 = UnknownInt28;
+            TempInstance.UnknownInt30 = UnknownInt30;
+            TempInstance.UnknownInt31 = UnknownInt31;
+            TempInstance.UnknownInt32 = UnknownInt32;
+
+            TempInstance.LTGState = LTGState;
+
+            TempInstance.Hash = Hash;
+            TempInstance.IncludeSound = IncludeSound;
+
+            if (IncludeSound)
+            {
+                InstanceJsonHandler.SoundData TempSound = new InstanceJsonHandler.SoundData();
+                TempSound.CollisonSound = Sounds.CollisonSound;
+
+                TempSound.ExternalSounds = new List<InstanceJsonHandler.ExternalSound>();
+
+                for (int i = 0; i < Sounds.ExternalSounds.Count; i++)
+                {
+                    var TempCollisionSound = new InstanceJsonHandler.ExternalSound();
+                    TempCollisionSound.U0 = Sounds.ExternalSounds[i].U0;
+                    TempCollisionSound.SoundIndex = Sounds.ExternalSounds[i].SoundIndex;
+                    TempCollisionSound.U2 = Sounds.ExternalSounds[i].U3;
+                    TempCollisionSound.U3 = Sounds.ExternalSounds[i].U3;
+                    TempCollisionSound.U4 = Sounds.ExternalSounds[i].U4;
+                    TempCollisionSound.U5 = Sounds.ExternalSounds[i].U5;
+                    TempCollisionSound.U6 = Sounds.ExternalSounds[i].U6;
+
+                    TempSound.ExternalSounds.Add(TempCollisionSound);
+                }
+
+
+                TempInstance.Sounds = TempSound;
+            }
+
+            TempInstance.U0 = U0;
+            TempInstance.PlayerBounceAmmount = PlayerBounceAmmount;
+            TempInstance.U2 = U2;
+            TempInstance.Visable = Visable;
+            TempInstance.PlayerCollision = PlayerCollision;
+            TempInstance.PlayerBounce = PlayerBounce;
+            TempInstance.Unknown241 = Unknown241;
+            TempInstance.UVScroll = UVScroll;
+
+            TempInstance.SurfaceType = ((int)SurfaceType) - 1;
+            TempInstance.CollsionMode = CollsionMode;
+            TempInstance.CollsionModelPaths = CollsionModelPaths;
+            TempInstance.U8 = U8;
+
+            TempInstance.EffectSlotIndex = EffectSlotIndex;
+            //if (EffectSlotObject != null)
+            //{
+            //    TempInstance.EffectSlotIndex = TrickyLevelManager.Instance.dataManager.GetEffectSlotID(EffectSlotObject);
+            //}
+            //else
+            //{
+            //    TempInstance.EffectSlotIndex = -1;
+            //}
+
+            TempInstance.PhysicsIndex = PhysicsIndex;
+            //if (PhysicsObject != null)
+            //{
+            //    TempInstance.PhysicsIndex = TrickyLevelManager.Instance.dataManager.GetPhysicstID(PhysicsObject);
+            //}
+            //else
+            //{
+            //    TempInstance.PhysicsIndex = -1;
+            //}
+
+
+            return TempInstance;
         }
 
         #region Rendering
