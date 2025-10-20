1f6940733b49631ff4e3542af7ae6b4e423bbdf8
GlitcherOG <54088472+GlitcherOG@users.noreply.github.com>
2025-10-20 18:26:21 +1100

    RayWarp Mesh/Material

---
+++ RayWarp/MaterialRef.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IceSaw2.RayWarp
+{
+    public class MaterialRef
+    {
+        public Raylib_cs.Material Material;
+        public MaterialRef(Raylib_cs.Material material)
+        {
+            Material = material;
+        }
+    }
+}
